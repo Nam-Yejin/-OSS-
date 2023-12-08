0badaa4d328dbf8747683fa1307dcc654d3cf8c6
JIN <144201064+Nam-Yejin@users.noreply.github.com>
2023-12-08 22:17:43 +0900

    Create 사용자 기본설정.py

---
+++ 사용자 기본설정.py
@@ -0,0 +1,9 @@
+$ git config --global user.name Nam-Yejin   - 사용자 이름설정
+$ git config --global user.email nam234400@gmail.com    -사용자 이메일 설정
+$ git config --global core.autocrlf true
+$ git config --global core.safecrlf false
+$ git config --global init.defaultBranch main
+
+----------------------------------------------------------------
+$ git init gfile   - 폴더 생성(gfile 폴더생성코드)
+$ cd gfile         -폴더 이동(gfile 폴더이동코드)
